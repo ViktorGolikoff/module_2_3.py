8041e6d9a3f7bed8beceaf385e3a92c7a088e9e4
ViktorGolikoff <pravgolvik@gmail.com>
2024-07-09 06:32:32 +0900

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,12 @@
 my_list = [42,69,322,13,0,99,-5,9,8,7,-6,5]
 start_index= 0
 while start_index<len(my_list):
-    if my_list[start_index] >= 0:
+    if my_list[start_index] > 0:
         print(my_list[start_index])
         start_index += 1
-    else:
+    elif my_list[start_index] == 0:
+        start_index += 1
+    elif my_list[start_index] < 0:
         break
 
 
